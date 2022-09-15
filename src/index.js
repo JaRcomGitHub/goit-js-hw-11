007357fa3152c8cc27fd7d7ec62a6e3fdf6c421d
JaRcomGitHub <jarcom@ukr.net>
2022-09-15 22:41:43 +0300

    js_dz11_v2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import './css/styles.css';
 import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const API_KEY = "29840548-44be53550e175681813a70adf";
 const PER_PAGE = 40;
@@ -11,11 +13,12 @@ const galleryEl = document.querySelector('div.gallery');
 const btnLoadMoreEl = document.querySelector('button.load-more');
 
 formEl.addEventListener('submit', onFormSubmit);
+galleryEl.addEventListener('click', onImageGalleryClick);
 btnLoadMoreEl.addEventListener('click', onClickLoadMore);
 
 btnLoadMoreEl.style.display = 'none';
-let pageCnt;
-let requestTerm;
+let glPageCnt;
+let glRequestTerm;
 
 function getPixabayURL(searchTerm, pageNum) {
   const basePixabayURL = "https://pixabay.com/api/";
@@ -52,12 +55,12 @@ function onFormSubmit(evt) {
     return;
   }
 
-  pageCnt = 1;
-  requestTerm = requestValue;
+  glPageCnt = 1;
+  glRequestTerm = requestValue;
   btnLoadMoreEl.style.display = 'none';
   galleryEl.textContent = "";
 
-  const url = getPixabayURL(requestTerm, pageCnt);
+  const url = getPixabayURL(glRequestTerm, glPageCnt);
 
   superfun(url);
 }
@@ -75,29 +78,28 @@ function superfun(url) {
         Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
         return;
       }
-      if (pageCnt === 1) {
+      if (glPageCnt === 1) {
         Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`); 
         if (totalHits > PER_PAGE) {
           btnLoadMoreEl.style.display = 'block';
         }
       }
 
-      //каждая открывается модалкой и есть стрелочки просмотра в модалке
-
       const markup = data.hits.map(data => markupPhotoCard(data)).join('');
       galleryEl.insertAdjacentHTML("beforeend", markup);
 
-      if ((pageCnt > 1)&&(data.hits.length < PER_PAGE)) {
+      useForGallerySimpleLightbox();
+
+      if ((glPageCnt > 1)&&(data.hits.length < PER_PAGE)) {
         Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
         btnLoadMoreEl.style.display = 'none';
       }
   })
   .catch(error => {
     console.log(error);
-    // if (error.message == 404) {
-    // }
+    // if (error.message == 404) { }
     if (error.message == 400) {
-      if (pageCnt > (PHOTO_LIMIT/PER_PAGE)) {
+      if (glPageCnt > (PHOTO_LIMIT/PER_PAGE)) {
         Notiflix.Notify.info("The API is limited to return a maximum of 500 images per query.");
         btnLoadMoreEl.style.display = 'none';
       }
@@ -106,8 +108,8 @@ function superfun(url) {
 }
 
 function onClickLoadMore() {
-  pageCnt += 1;
-  const url = getPixabayURL(requestTerm, pageCnt);
+  glPageCnt += 1;
+  const url = getPixabayURL(glRequestTerm, glPageCnt);
   superfun(url);
 }
 
@@ -141,21 +143,16 @@ function markupPhotoCard(
   `;
 }
 
+function onImageGalleryClick(event){
+    event.preventDefault();
+}
+
+function useForGallerySimpleLightbox(){
+    new SimpleLightbox('.gallery a');
+}
+
 // axios.get('/users')
 //   .then(res => {
 //     console.log(res.data);
 //   });
-
-
-// В ответе будет массив изображений удовлетворивших критериям параметров запроса.
-// Каждое изображение описывается объектом, из которого тебе интересны только следующие свойства:
-
-// Если бэкенд возвращает пустой массив, значит ничего подходящего найдено небыло.
-// В таком случае показывай уведомление с текстом
-// "Sorry, there are no images matching your search query. Please try again.". 
-// Для уведомлений используй библиотеку notiflix.
-
-
-//https://pixabay.com/api/?key=29840548-44be53550e175681813a70adf& q=yellow & image_type=photo & orientation=horizontal & safesearch=true&page=1
-//
 
